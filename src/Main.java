7c6a54a49ad25e74532d2d689b1a5c1e01ccdd50
theKJEngineer <kjanas.it@gmail.com>
2025-10-11 19:29:42 +0200

    Initial commit - project structure and basic calculator class

---
+++ src/Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+class Calculator {
+    private double firstNumber;
+    private double secondNumber;
+    private double result;
+    private String operator;
+
+    public Calculator() {
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+        this.operator = "";
+    }
+
+    public void menu() {
+        System.out.println("=========================");
+        System.out.println("| Calculator version 1.0 |");
+        System.out.println("| Beginning of Java OOP  |");
+        System.out.println("=========================");
+    }
+
+    public void showOperationMenu() {
+        System.out.println("Select the mathematical operation you want to perform.");
+
+    }
+
+    public double add() {
+         result = firstNumber + secondNumber;
+         return result;
+    }
+
+    public double subtract() {
+        result = firstNumber - secondNumber;
+        return result;
+    }
+
+    public double multiply() {
+        result = firstNumber * secondNumber;
+        return result;
+
+    }
+
+    public double divide() {
+        if (secondNumber == 0) {
+            System.out.println("Division by 0 is not allowed.");
+        }
+        result = firstNumber / secondNumber;
+        return result;
+    }
+
+    public double getResult() {
+        return result;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator c1 = new Calculator();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double a = scanner.nextDouble();
+        System.out.println("Enter second number: ");
+        double b = scanner.nextDouble();
+
+
+
+
+
+    }
+}
